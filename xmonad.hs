78b47cd67ec8eb40e7962aeb94ec239129b94812
Iain Ballard <iain.ballard@gmail.com>
2013-08-29 18:01:36 +0100

    shift-win-z to lock screen

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,15 @@
 import XMonad
+import XMonad.Util.EZConfig(additionalKeys)
 
-main = xmonad defaultConfig
-              { modMask = mod4Mask  -- use win key as window modifier
-              , terminal = "urxvt"
-              }
+-- very basic xmonad
+main = xmonad myConfig
+
+myConfig =
+    defaultConfig
+        { modMask = mod4Mask  -- use win key as window modifier
+        , terminal = "urxvt"
+        } `additionalKeys`
+        [
+        ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        ]
 
